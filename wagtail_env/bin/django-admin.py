a632e3de011e053717a55e3727b446e8f910d4cd
Guillaume Ah-ki <guillaumeahki@gmail.com>
2019-04-24 10:12:49 -0400

    added multi site functionnality

--- wagtail_env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/guillaumeah-ki/Documents/django-wagtail-sample/wagtail_env/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
